6e8cedea40741af57012050efbbf2cbdc262c572
haoyan.zhang <haoyan.zhang@qunar.com>
2018-09-01 19:59:31 +0800

    批作业“

--- helloworld/src/helloworld.java
+++ helloworld/src/helloworld.java
@@ -1,5 +1,11 @@
+
+//命名 类名要用大驼峰 HelloWorld
 public class helloworld {
     public static void main(String[] args){
         System.out.println("Hello World!");
     }
 }
+
+//要建个包，不要直接放在src下
+//包名一般是公司域名倒着写  如baidu.com就会以com.baidu开头
+//建个包名com.lilanjie 把代码放到包里
