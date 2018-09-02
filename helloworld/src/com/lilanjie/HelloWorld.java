dbb8ae883035b4421adc6f53801ced9e634c6973
lilanjie <18512448197@163.com>
2018-09-02 12:02:39 +0800

    作业修改
    作业修改

--- helloworld/src/com/lilanjie/HelloWorld.java
+++ helloworld/src/com/lilanjie/HelloWorld.java
@@ -1,6 +1,7 @@
+package com.lilanjie;
 
 //命名 类名要用大驼峰 HelloWorld
-public class helloworld {
+public class HelloWorld {
     public static void main(String[] args){
         System.out.println("Hello World!");
     }
